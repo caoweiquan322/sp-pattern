d9692aacb4737363b18e16d99d1948ac0fb54825
caoweiquan322 <caoweiquan322@126.com>
2016-06-09 22:46:31 +0800

    Added script for mac os x.

---
+++ scripts/mac.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+APP=../build-st_pattern-Desktop_Qt_5_4_1_clang_64bit-Release/st_pattern.app/Contents/MacOS/st_pattern
+
+INTER_FILE_NAME=test
+
+SEG_DATA="../test_files"
+SEG_SUFFIX=.txt
+SEG_STEP=1.6
+SEG_USE_SED=1
+SEG_MIN_LEN=10
+
+CLU_WEIGHT="0.001:0.001:0.001:0.001:0:0"
+CLU_THRESH=0.0004
+CLU_MEM_LIM=100
+
+MINE_RADIUS=150.0
+MINE_MIN_SUP=10
+MINE_MIN_PAT_LEN=3
+
+
+$APP seg $SEG_DATA $SEG_SUFFIX $INTER_FILE_NAME $SEG_STEP $SEG_USE_SED $SEG_MIN_LEN
+
+$APP cluster $INTER_FILE_NAME $CLU_WEIGHT $INTER_FILE_NAME $CLU_THRESH $CLU_MEM_LIM
+
+$APP mine $INTER_FILE_NAME $INTER_FILE_NAME $INTER_FILE_NAME $MINE_RADIUS $MINE_MIN_SUP $MINE_MIN_PAT_LEN
+
+rm *.cluster
+rm *.s2c
+rm *.seg
+rm *.stp
+rm *.tinc
+rm *.tins
